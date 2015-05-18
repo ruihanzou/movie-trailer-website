2dae1ff323ce766fac4e563e7acadcf263f1bfce
ruihanzou <stefan.ruihan@gmail.com>
2015-05-18 08:27:42 -0500

    Update media.py
    add docStrings

--- media.py
+++ media.py
@@ -4,10 +4,16 @@ import webbrowser
 class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube):
+        """
+        A Movie class with title, storyline, image and trailer attributes.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        You can watch a movie trailer by calling show_trailer method.
+        """
         webbrowser.open(self.trailer_youtube_url)
